18f56a7da9b2ebb3407692baf40747bc01e3ac39
yashrawat <yashrawat682@gmail.com>
2024-04-24 18:32:29 +0530

    Created the Schema of DataBase

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose")
+const { boolean } = require("zod")
+
+mongoose.connect("mongodb+srv://thinkofyash:18NovMonday@cluster0.xay1cqj.mongodb.net/TaskSphereDb")
+
+const todoSchema=mongoose.Schema({
+    title:String,
+    description:String,
+    completed:Boolean
+})
+
+
+const todo=mongoose.model("todos",todoSchema)
+
+
+module.exports={
+    todos:todo
+}
---
+++ Backend/index.js
@@ -0,0 +1,45 @@
+const express=require("express")
+const bodyParser=require("body-parser")
+const { string } = require("zod")
+const { createTodo, updateTodo } = require("./types")
+
+const app=express()
+app.use(express.json())
+//body {
+  //  title:string
+   // description:String
+//}
+app.post("/todo",function(req,res){
+    const newtodo=req.body.newtodo
+    const validatetodo=createTodo.safeParse(newtodo)
+    if(!validatetodo.success){
+        res.status(411).json({
+            msg:"Invalid Todo  "
+        })
+        return ;
+    }else{
+
+    }
+
+
+})
+
+app.get("/todos",function(req,res){
+
+})
+app.put("/completed",function(req,res){
+    const todoId=req.body.todoId
+    const validateiD=updateTodo.safeParse(todoId)
+    if(!validateiD.success){
+        res.status(411).json({
+            msg:"Invalid Todo id"
+        })
+        return ;
+    }else{
+
+    }
+
+
+})
+
+app.listen(3000)
