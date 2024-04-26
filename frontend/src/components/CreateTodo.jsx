b0c3cd32d1bda0bc54ad9c92469a3dd92348c2ed
yashrawat <yashrawat682@gmail.com>
2024-04-26 07:43:18 +0530

    when the button is Clicked it sends Post request to add Todo

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,21 +1,45 @@
+import { useState } from "react"
+
 export function CreateTodo(){
+    const [title,setTitle]=useState("")
+    const [description,setDescription]=useState("")
     return <div>
 
         <input style={{
             padding:10,
             margin:10
-        }} type="text" placeholder="Title" /> <br />
-        
+        }} type="text" placeholder="Title" onChange={function(e){
+            const value=e.target.value
+            setTitle(e.target.value)
+        }} /> <br />
+
         <input style={{
             padding:10,
             margin:10
-        }} type="text" placeholder="Description" /> <br />
+        }} type="text" placeholder="Description" onChange={function(e){
+            const value=e.target.value
+            setDescription(e.target.value)
+        }} /> <br />
+
+      
 
         <button style={{
             padding:10,
             margin:10,
             
-        }} >Add a ToDo</button>
+        }} onClick={()=>{fetch("http://localhost:3000/todo",{
+            method:"POST",
+            body: JSON.stringify({
+                title:title,
+                description:description
+            }),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then(async function(e){
+            const data=await e.json()
+            alert("Todo Added")
+        })}} >Add a ToDo</button>
 
 
     </div>
